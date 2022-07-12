a9fb7ab54546424280d299abc296f07078b1f9b5
nao <altenyt@gmail.com>
2022-07-12 07:39:36 +0000

    Separate font awesome loading into a separate file

--- client/src/main.js
+++ client/src/main.js
@@ -1,14 +1,11 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-
-import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
-import { fab } from '@fortawesome/free-brands-svg-icons';
+import App from './App.vue'
 import router from './router';
 
-library.add(fas, far, fab);
+// setup Font Awesome.
+import fontAwesome from './fontAwesome';
+fontAwesome();
 
 createApp(App)
     .use(router)
