ad4f7a0035fc8f10b4c02cf35f0de912ae667f55
nao <altenyt@gmail.com>
2022-07-12 07:48:35 +0000

    Allow content to have 100% (or more) viewport height

--- client/src/App.vue
+++ client/src/App.vue
@@ -10,25 +10,37 @@ import NavbarRoute from './components/Navbar/Route.vue';
     <NavbarRoute name="Mods" icon="fa-solid fa-gear" url="/mods" />
     <NavbarRoute name="Manual" icon="fa-solid fa-book-open" url="/manual" />
   </Navbar>
-  <router-view></router-view>
+  <div class="view">
+    <router-view></router-view>
+  </div>
 </template>
 
 <style lang="less">
 @import "./utilities/reset.less";
 
+body, html {
+  height: 100%;
+}
+
 #app {
-    font-family: "Karla", Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    overflow-x: hidden;
+  font-family: "Karla", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  overflow-x: hidden;
+  height: 100%;
+}
+
+.view {
+  width: 100%;
+  height: calc(100% - 57.8px);
 }
 
 h1 {
-    font-family: "Krona One", Helvetica, Arial, sans-serif;
+  font-family: "Krona One", Helvetica, Arial, sans-serif;
 }
 
 a {
-    color: inherit;
-    text-decoration: none;
+  color: inherit;
+  text-decoration: none;
 }
 </style>
